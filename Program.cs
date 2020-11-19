1acaf0b88d8b50a60db11c28eb781563b736c3c8
Maiara Evangelista <maiaraevangelista933@gmail.com>
2020-11-19 16:06:00 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Estruturas_de_Repetição2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string nome;
+          string senha;
+
+            do{
+            Console.WriteLine("Bem Vindo! Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua senha: ");
+            senha = Console.ReadLine();
+            
+                if(senha == nome){
+                    Console.Write("Sua senha não pode ser a mesma que seu nome de usuário!");
+                }
+            } while(senha == nome);
+            Console.WriteLine("Senha Válida");
+           
+        }
+    }
+}
